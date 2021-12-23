d48d6e4592ae20930e73c7581e3c946ece9460e4
Kev1nR <kevinroche@blueyonder.co.uk>
2021-12-23 16:31:27 +0000

    Day 3 complete

--- Day3/Day3-pt2.fsx
+++ Day3/Day3-pt2.fsx
@@ -13,71 +13,97 @@ let getFlagCounts level readings =
     let binCounts =
         readings
         |> Seq.countBy (fun el -> el |> Seq.skip level |> Seq.head )
+    printfn "bincounts %A" binCounts
     binCounts
 
-let getMaxCount counts =
+let getCount precedenceValue op counts =
     counts
-    |> Seq.maxBy (fun el -> snd el)
-    |> fst
+    |> Seq.toList
+    |> fun cs ->
+        match cs with
+        | a::b::[] ->
+            if (snd a) = (snd b)
+            then
+                precedenceValue
+            elif op (snd a) (snd b)
+            then
+                fst a
+            else
+                fst b
+        | _ -> failwith "Unexpected char count"
 
-let rec getOxyGen level (readings : char[] seq) =
+
+let rec getOxyCOGen searchChar op level (readings : char[] seq) =
     let filterKey =
-        readings |> getFlagCounts level |> getMaxCount
+        readings |> getFlagCounts level |> getCount searchChar op
 
     let levelReadings =
         readings
         |> Seq.filter (fun reading -> reading[level] = filterKey)
 
-    match levelReadings with
-    | [] | h::[] -> levelReadings
-    | _ -> getOxyGen (level + 1) readings
+    printfn "%d: %d %A" level (levelReadings |> Seq.length) levelReadings
+    match levelReadings |> Seq.length with
+    | 0 | 1  -> levelReadings |> Seq.head
+    | _ -> getOxyCOGen searchChar op (level + 1) levelReadings
 
-let calcResult data =
-    let dataWidth = (data |> Seq.head |> String.length) - 1
-    let acc' = 0, [ for i in 0..dataWidth do yield 0 ]
+let getOxyGen level readings = getOxyCOGen '1' (>) level readings
+let getCOGen level readings = getOxyCOGen '0' (<) level readings
 
+let calcDecVal data =
     data
-    |> Seq.map (fun s -> s.ToCharArray()) //Seq.map (accumulate')
-    |> Seq.fold (fun acc el ->
-                        (fst acc) + 1,
-                        [
-                            for i in 0..dataWidth do yield (snd acc)[i] + (if el[i] = '1' then 1 else 0)
-                        ]
-                    ) acc'
-    |> fun (len, vals) -> [for i in 0..dataWidth do yield if vals[i] > (len/2) then 1 else 0]
-    |> List.rev
-    |> List.fold (fun (i,b,c) el ->
-                        let el' = if el = 1 then 0 else 1
-
-                        let summand = (float)el * (2.**(float i))
+    |> Array.rev
+    |> Array.fold (fun (i,b) el ->
+                        let el' = if el = '0' then 0 else 1
                         let summand' = (float)el' * (2.**(float i))
 
-                        (i + 1, b + (int summand), c + (int summand'))
-                 ) (0, 0, 0)
+                        let acc = (i + 1, b + (int summand'))
+                        printfn "acc %A" acc
+                        acc
+                 ) (0, 0)
 
 //(ReadData.readLines filePath)
-[
-    "00100"
-    "11110"
-    "10110"
-    "10111"
-    "10101"
-    "01111"
-    "00111"
-    "11100"
-    "10000"
-    "11001"
-    "00010"
-    "01010"
-]
-|> splitString
-|> getOxyGen 0
+let oxy () =
+    [
+        "00100"
+        "11110"
+        "10110"
+        "10111"
+        "10101"
+        "01111"
+        "00111"
+        "11100"
+        "10000"
+        "11001"
+        "00010"
+        "01010"
+    ]
+    |> splitString
+    |> getOxyGen 0
+    |> calcDecVal
+
+let CO () =
+    [
+        "00100"
+        "11110"
+        "10110"
+        "10111"
+        "10101"
+        "01111"
+        "00111"
+        "11100"
+        "10000"
+        "11001"
+        "00010"
+        "01010"
+    ]
+    |> splitString
+    |> getCOGen 0
+    |> calcDecVal
+
+// |> getFlagCounts
+// |> getMaxCount
 
 
-|> getFlagCounts
-|> getMaxCount
 
-
-
-|> calcResult
-|> fun (_, l, r) -> l * r
+// |> calcResult
+// |> fun (_, l, r) -> l * r
